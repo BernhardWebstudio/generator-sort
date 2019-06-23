c569936e867f96a2bb0a8467bd544c59a09e97c7
Mattias Holmlund <mattias@holmlund.se>
2019-06-23 13:20:46 +0200

    Improved types

--- index.ts
+++ index.ts
@@ -2,11 +2,13 @@ export function compareStrings(sA: string, sB: string) {
     return [sA, sB]
 }
 
-type CompareFunction = (a: any, b: any) => -1 | 0 | 1
+type CompareFunction<T> = (a: T, b: T) => -1 | 0 | 1
 
-export function sortGenerator(_gen: any): CompareFunction {
-    return (a: any, b: any) => {
-        for (let comp of _gen(a, b)) {
+export function sortGenerator<T>(
+    gen: (a: T, b: T) => IterableIterator<string[]>
+): CompareFunction<T> {
+    return (a: T, b: T) => {
+        for (let comp of gen(a, b)) {
             if (comp[0] < comp[1]) {
                 return -1
             } else if (comp[0] > comp[1]) {
--- test/test-basic.ts
+++ test/test-basic.ts
@@ -16,8 +16,13 @@ test('sort strings', async function(t) {
     )
 })
 
+interface Obj {
+    k1: string
+    k2: string
+}
+
 test('sort objects', async function(t) {
-    const input = [
+    const input: Obj[] = [
         {
             k1: 'b',
             k2: 'f',
@@ -32,26 +37,25 @@ test('sort objects', async function(t) {
         },
     ]
 
-    t.deepEqual(
-        input.sort(
-            sortGenerator(function*(oA: any, oB: any) {
-                yield compareStrings(oA.k1, oB.k1)
-                yield compareStrings(oA.k2, oB.k2)
-            })
-        ),
-        [
-            {
-                k1: 'a',
-                k2: 'f',
-            },
-            {
-                k1: 'b',
-                k2: 'e',
-            },
-            {
-                k1: 'b',
-                k2: 'f',
-            },
-        ]
+    const output = input.sort(
+        sortGenerator(function*(oA: Obj, oB: Obj) {
+            yield compareStrings(oA.k1, oB.k1)
+            yield compareStrings(oA.k2, oB.k2)
+        })
     )
+
+    t.deepEqual(output, [
+        {
+            k1: 'a',
+            k2: 'f',
+        },
+        {
+            k1: 'b',
+            k2: 'e',
+        },
+        {
+            k1: 'b',
+            k2: 'f',
+        },
+    ])
 })
